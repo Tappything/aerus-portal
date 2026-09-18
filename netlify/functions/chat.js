9d38709191bde341c6d8707d470d8f472069f570
Tappything <william@towsonhealthyhomes.com>
2026-09-18 05:55:17 -0400

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -28,10 +28,11 @@ exports.handler = async (event) => {
     return new Promise((resolve) => {
       const req = https.request({
         hostname: 'generativelanguage.googleapis.com',
-        path: '/v1beta/models/gemini-1.5-flash:generateContent?key=' + apiKey,
+        path: '/v1beta/models/gemini-1.5-flash:generateContent',
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + apiKey,
           'Content-Length': Buffer.byteLength(postData)
         }
       }, (res) => {
